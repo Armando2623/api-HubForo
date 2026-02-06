07002d47686191843338f6498789a92d6bdc784b
Armando <armando.ortizvegas@gmail.com>
2026-02-06 00:18:15 -0500

    Proyecto backend para un foro

---
+++ src/test/java/com/ortiz/foroHub/ForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ortiz.foroHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
